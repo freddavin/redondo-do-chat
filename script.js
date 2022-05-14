96c6febacfcf6a94f0353ef7e3b6ee8ce900a5d4
Fred Davin <fredericodavin@gmail.com>
2022-05-14 10:48:29 -0300

    modificação no resultado de alteração de pick

--- script.js
+++ script.js
@@ -6,26 +6,39 @@ function allowDrop(ev) {
 }
 
 function drag(ev) {
-    //console.log(ev.target.id);    
     if (boxImgs.findIndex(box => box.name === ev.target.id) === -1) {
-        boxImgs.push({name: ev.target.id, count: 1});
+        boxImgs.push({ name: ev.target.id, count: 1 });
     }
     index = boxImgs.findIndex(box => box.name === ev.target.id);
     ev.dataTransfer.setData("text", ev.target.id);
 }
 
 function drop(ev) {
-    //console.log(ev.target.id);
     ev.preventDefault();
     let data = ev.dataTransfer.getData("text");
-    if ("box" + data === ev.target.id || ev.target.id === "pickBox" && boxImgs[index].count > 0) {
-        //ev.target.appendChild(document.getElementById(data));
+    //console.log(data); // origem
+    //console.log(ev.target.id); // destino
+    if (ev.target.id === "pickBox" && boxImgs[index].count > 0) {
         let nodeCopy = document.getElementById(data).cloneNode(true);
-        //nodeCopy.id = "newId";
+        nodeCopy.id = document.getElementById(data).id + "Copy";
         ev.target.appendChild(nodeCopy);
         boxImgs[index].count--;
-    } else if (data === ev.target.id) {
-        ev.dataTransfer.getElementById(data).clearData();
+        document.getElementById(data).style.opacity = 0.2;
+        console.log("1");
     }
-    
+}
+
+function dragend(ev) {
+    ev.preventDefault();
+    if (ev.target.id.substr(-4) === "Copy") {
+        ev.target.parentNode.removeChild(ev.target);
+        addCount(ev);
+        document.getElementById(ev.target.id.substr(0, ev.target.id.length - 4)).style.opacity = 1; // colocar condição para quando mudar de picbox
+        console.log("2");
+    }
+}
+
+function addCount(ev) {
+    index = boxImgs.findIndex(box => box.name === ev.target.id.substr(0, ev.target.id.length - 4));
+    boxImgs[index].count++;
 }
